c43f53d9cc7782733f8c33f0d9716a32a5ed0ecd
Charlie Dovey <CharlieDovey98@hotmail.co.uk>
2024-11-28 17:46:58 +0000

    Updated image file pathing syntax errors.

--- Javascript/vue.js
+++ Javascript/vue.js
@@ -101,7 +101,7 @@ let app = new Vue({
     // Below are reusable methods for API calls and event handling.
 
     // A method for attaining the images full url, to then display it on the website.
-    getFullImageUrl(imagePath) {// Method to correct the URL to display the image from the server
+    getFullImageUrl(imagePath) {
       const serverUrl = "https://superschoolstore.eu-west-2.elasticbeanstalk.com/";
       return serverUrl + imagePath;
     },
